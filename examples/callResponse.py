0dc4c714713696daf63d38d5d7564a514dc75a02
matsfunk <mathias.funk@gmail.com>
2017-07-09 08:48:57 +0800

    added call-response functionality (“synchronous send”) added callResponse example

---
+++ examples/callResponse.py
@@ -0,0 +1,42 @@
+import time
+from oocsi import OOCSI
+
+def respondToEvent(response):
+    # set data field in the response
+    response['newColor'] = int(response['oldColor']) + 1
+    
+    # play with this delay to let the caller time out
+    # time.sleep(4)
+
+# start responder OOCSI client
+responder = OOCSI('callResponseResponder', 'localhost', 4444)
+# register responder
+responder.register('colorChannel', 'colorGenerator', respondToEvent)
+
+
+### test colorGenerator with two calls
+
+# start caller OOCSI client
+caller = OOCSI('callResponseSender', 'localhost', 4444)
+
+# asynchronous call
+call1 = caller.call('colorChannel', 'colorGenerator', {'oldColor': 9}, 1)
+# wait for 1 sec
+time.sleep(1)
+
+# check response in call object
+if 'response' in call1:
+    print(call1['response'])
+else:
+    print('response not found')
+
+# blocking call
+call2 = caller.callAndWait('colorChannel', 'colorGenerator', {'oldColor': 19}, 1)
+# check response in call object directly
+if 'response' in call2:
+    print(call2['response'])
+else:
+    print('response not found')
+
+caller.stop()
+responder.stop()
--- oocsi/__init__.py
+++ oocsi/__init__.py
@@ -2,6 +2,7 @@ import json
 import socket
 import threading
 import time
+import uuid
 
 __author__ = 'matsfunk'
 
@@ -10,13 +11,15 @@ class OOCSI:
     
     def __init__(self, handle, host='localhost', port=4444, callback=None):
         self.receivers = {handle: callback}
+        self.calls = {}
+        self.services = {}
         self.reconnect = True
         self.connected = False
         
         # Connect the socket to the port where the server is listening
         self.server_address = (host, port)
         self.handle = handle
-        print('connecting to %s port %s' % self.server_address)
+        self.log('connecting to %s port %s' % self.server_address)
         
         # start the connection thread        
         self.runtime = OOCSIThread(self)
@@ -39,7 +42,7 @@ class OOCSI:
             
                 data = self.sock.recv(1024)
                 if data.startswith('{'):  
-                    print('connection established for ' + message)
+                    self.log('connection established')
                     # re-subscribe for channels
                     for channelName in self.receivers:
                         self.internalSend('subscribe {0}'.format(channelName))
@@ -53,7 +56,9 @@ class OOCSI:
         
         except: 
             {}
-        
+    
+    def log(self, message):
+        print('[{0}]: {1}'.format(self.handle, message))
         
     def internalSend(self, msg):
         self.sock.sendall(msg + '\n')
@@ -81,23 +86,73 @@ class OOCSI:
         del event['recipient']
         del event['sender']
         del event['timestamp']
-        del event['data']
+        if 'data' in event:
+            del event['data']
         
-        if recipient in self.receivers and self.receivers[recipient] != None:
-            self.receivers[recipient](sender, recipient, event)
+        if '_MESSAGE_HANDLE' in event and event['_MESSAGE_HANDLE'] in self.services:
+            service = self.services[event['_MESSAGE_HANDLE']]
+            del event['_MESSAGE_HANDLE']
+            service(event)
+            self.send(sender, event)
+            self.receiveChannelEvent(sender, recipient, event)
+        
+        else:
+            
+            if '_MESSAGE_ID' in event:
+                myCall = self.calls[event['_MESSAGE_ID']]
+                if myCall['expiration'] > time.time():
+                    response = self.calls[event['_MESSAGE_ID']]
+                    response['response'] = event
+                    del response['expiration']
+                    del response['_MESSAGE_ID']
+                    del response['response']['_MESSAGE_ID']
+                else:
+                    del self.calls[event['_MESSAGE_ID']]
+                    
+            else:
+                self.receiveChannelEvent(sender, recipient, event)
 #         else:
 #             print('not good -------------- ', filteredEvent)
+
+    def receiveChannelEvent(self, sender, recipient, event):
+        if recipient in self.receivers and self.receivers[recipient] != None:
+            self.receivers[recipient](sender, recipient, event)
 
     def send(self, channelName, data):
         self.internalSend('sendraw {0} {1}'.format(channelName, json.dumps(data)))
 
+    def call(self, channelName, callName, data, timeout = 1):
+        data['_MESSAGE_HANDLE'] = callName 
+        data['_MESSAGE_ID'] = uuid.uuid4().__str__()
+        self.calls[data['_MESSAGE_ID']] = {'_MESSAGE_HANDLE': callName, '_MESSAGE_ID': data['_MESSAGE_ID'], 'expiration': time.time() + timeout}
+        self.send(channelName, data)
+        return self.calls[data['_MESSAGE_ID']]
+        
+
+    def callAndWait(self, channelName, callName, data, timeout = 1):
+        call = self.call(channelName, callName, data, timeout)
+        expiration = time.time() + timeout
+        while time.time() < expiration:
+            time.sleep(0.1)
+            if 'response' in call:
+                break;
+            #         self.calls.append
+        return call
+
+    def register(self, channelName, callName, callback):
+        self.services[callName] = callback
+        self.internalSend('subscribe {0}'.format(channelName))
+        self.log('registered responder on {0} for {1}'.format(channelName, callName))
+
     def subscribe(self, channelName, f):
         self.receivers[channelName] = f
         self.internalSend('subscribe {0}'.format(channelName))
+        self.log('subscribed to {0}'.format(channelName))
     
     def unsubscribe(self, channelName):
         del self.receivers[channelName]
         self.internalSend('unsubscribe {0}'.format(channelName))
+        self.log('unsubscribed from {0}'.format(channelName))
 
     def stop(self):
         self.reconnect = False
@@ -118,11 +173,23 @@ class OOCSIThread(threading.Thread):
         while self.parent.reconnect:
             self.parent.init()
             if self.parent.reconnect:
-                print('re-connecting to OOCSI')
+                self.parent.log('re-connecting to OOCSI')
                 time.sleep(5)
 
-        print('closing connection to OOCSI')
+        self.parent.log('closing connection to OOCSI')
 
     def __stop(self):
         self.parent.stop()
         return threading.Thread.__stop(self) 
+
+
+class OOCSICall:
+    
+    def __init__(self, parent=None):
+        self.uuid = uuid.uuid4()
+        self.expiration = time.time()
+        
+
+
+
+        
