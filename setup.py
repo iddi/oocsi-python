27fb884dc8ddfa500f9b04f486e3c0ff4be5b2f5
matsfunk <mathias.funk@gmail.com>
2017-07-10 11:05:48 +0800

    version 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="oocsi",
-    version="0.1",
+    version="0.2",
     description="Python Client for OOCSI",
     author="matsfunk",
     author_email="m.funk@tue.nl",
