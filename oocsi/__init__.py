dc00722e7e6aba1cb9e623b76763212cc579f4fe
Cyanogenbot <diedeasus@gmail.com>
2021-09-14 13:05:28 +0200

    changed raise position

--- oocsi/__init__.py
+++ oocsi/__init__.py
@@ -190,7 +190,6 @@ class OOCSI:
         {}
 
 class OOCSIThread(threading.Thread):
-
     def __init__(self, parent=None):
         self.parent = parent
         super(OOCSIThread, self).__init__()
@@ -201,7 +200,7 @@ class OOCSIThread(threading.Thread):
             if self.parent.reconnect:
                 self.parent.log('re-connecting to OOCSI')
                 time.sleep(5)
-                raise OOCSIDisconnect('Oocsi has not been found')
+            raise OOCSIDisconnect('Oocsi has not been found')
 
         self.parent.log('closing connection to OOCSI')
 
